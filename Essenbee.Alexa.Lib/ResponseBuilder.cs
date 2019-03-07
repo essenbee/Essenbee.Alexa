3c049ab8807de702931011bb32372169a4bf9264
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2019-03-07 22:33:27 +0000

    Episode 6

--- Essenbee.Alexa.Lib/ResponseBuilder.cs
+++ Essenbee.Alexa.Lib/ResponseBuilder.cs
@@ -5,18 +5,19 @@ using System.Text;
 
 namespace Essenbee.Alexa.Lib
 {
-    public class SkillResponse
+    public class ResponseBuilder
     {
         private AlexaResponse _response;
 
-        public SkillResponse()
+        public ResponseBuilder()
         {
             _response = new AlexaResponse();
         }
 
-        public SkillResponse AddTextSpeech(string text)
+        public ResponseBuilder Say(string text)
         {
             _response.Response = new ResponseBody();
+            _response.Response.ShouldEndSession = true;
             _response.Response.OutputSpeech = new TextOutput(text);
             return this;
         }
--- Essenbee.Alexa/Controllers/AlexaController.cs
+++ Essenbee.Alexa/Controllers/AlexaController.cs
@@ -20,17 +20,85 @@ namespace Essenbee.Alexa.Controllers
             // ToDo: ensure that request is meant for us
 
             AlexaResponse response = null;
-            var responseBuilder = new SkillResponse();
+            var responseBuilder = new ResponseBuilder();
 
             switch (alexaRequest.RequestBody.Type)
             {
                 case "LaunchRequest":
-                    response = responseBuilder.AddTextSpeech("Welcome to the Dev Streams skill")
+                    response = responseBuilder.Say("Welcome to the Dev Streams skill")
                         .Build();
+                    break;
+                case "IntentRequest":
+                    response = IntentRequestHandler(alexaRequest);
                     break;
                 default:
                     break;
             }
+
+            return response;
+        }
+
+        private AlexaResponse IntentRequestHandler(AlexaRequest alexaRequest)
+        {
+            var intentRequest = alexaRequest.RequestBody as IntentRequest;
+
+            //var response = new ResponseBuilder()
+            //    .Say($"I received an intent request {intentRequest.Intent.Name}")
+            //    .Build();
+
+
+            AlexaResponse response = null;
+
+            if (intentRequest != null)
+            {
+                switch (intentRequest.Intent.Name)
+                {
+                    case "devstreams":
+                        response = DevStreamsResponseHandler(intentRequest);
+                        break;
+                    case "AMAZON.StopIntent":
+                    case "AMAZON.CancelIntent":
+                        response = CancelOrStopResponseHandler(intentRequest);
+                        break;
+                    case "AMAZON.HelpIntent":
+                        response = HelpIntentResponseHandler(intentRequest);
+                        break;
+                }
+            }
+
+            return response;
+        }
+
+        private AlexaResponse HelpIntentResponseHandler(IntentRequest intentRequest)
+        {
+            var response = new ResponseBuilder()
+                .Say("To use this skill, ask me about the schedule of your favourite stream. " +
+                "You can also say Alexa stop to exit the skill")
+                .Build(); ;
+            return response;
+        }
+
+        private AlexaResponse CancelOrStopResponseHandler(IntentRequest intentRequest)
+        {
+            var response = new ResponseBuilder()
+                .Say("Thanks for using the Dev Streams skill")
+                .Build();
+
+            return response;
+        }
+
+        private AlexaResponse DevStreamsResponseHandler(IntentRequest intentRequest)
+        {
+            var channel = "some streamer";
+
+            if (intentRequest.Intent.Slots.Any())
+            {
+                channel = intentRequest.Intent.Slots["channel"].Value;
+            }
+
+            var response = new ResponseBuilder()
+                .Say($"You have asked about {channel} and their schedule")
+                .Build();
 
             return response;
         }
